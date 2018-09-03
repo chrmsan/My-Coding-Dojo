9870a18cd9477c760ad1ce28868454349d2a4bc8
Christopher Sanchez <sanchez.christopherm@gmail.com>
2018-09-03 20:39:55 +0200

    Add first test for CodeCrackers class

--- Java/Code Cracker/test/CodeCracker/CodeCrackerTest.java
+++ Java/Code Cracker/test/CodeCracker/CodeCrackerTest.java
@@ -2,13 +2,22 @@ package CodeCracker;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CodeCrackerTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Encrypt a single alphabetic character")
+	void returnACharacter() {
+		CodeCracker codecracker = new CodeCracker();
+		String character = "a";
+		
+		String result = codecracker.encrypt(character);
+		String expectedResult = "!";
+		
+		assertEquals(expectedResult, result);
+		
 	}
 
 }
