52745239d2f9da4dcd25756eedf9a466bc01ad6a
Christopher Sanchez <sanchez.christopherm@gmail.com>
2018-09-03 21:00:10 +0200

    First test pass

---
+++ Java/Code Cracker/src/CodeCracker/CodeCracker.java
@@ -0,0 +1,16 @@
+package CodeCracker;
+
+public class CodeCracker {
+	
+	public String encrypt(String character) {
+		String output = "";
+		
+		if (character == "a") {
+			output = "!";
+		}
+		
+		return output;
+	}
+	
+	
+}
