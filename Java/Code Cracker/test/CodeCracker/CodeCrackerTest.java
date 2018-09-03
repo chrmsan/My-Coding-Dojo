34bb3096c50eca9aa672a43d5edc31d06ddc9cef
Christopher Sanchez <sanchez.christopherm@gmail.com>
2018-09-03 20:26:38 +0200

    Change previous folder to Code Cracker

---
+++ Java/Code Cracker/test/CodeCracker/CodeCrackerTest.java
@@ -0,0 +1,14 @@
+package CodeCracker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CodeCrackerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
